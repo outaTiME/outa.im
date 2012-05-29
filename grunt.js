b9163ff8e002dee61e44f9cd3ce7049934478e29
outaTiME <afalduto@gmail.com>
2012-05-28 21:12:30 -0300

    Version bump.

--- grunt.js
+++ grunt.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     meta: {
-      version: '0.9.1',
+      version: '0.9.2a',
       banner: '/*!\n' +
         '*              __     _\n' +
         '*   _    _/__  /./|,//_`\n' +
