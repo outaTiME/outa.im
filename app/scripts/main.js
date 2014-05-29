95a3be582c6c83dfb7de731e29fb2d7d2cc930ac
outaTiME <afalduto@gmail.com>
2014-05-28 21:21:33 -0300

    Earlier commit for new site version.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,58 @@
 
 'use strict';
 
-var hasTouch = 'ontouchstart' in window;
+$(document).ready(function () {
 
-$('.cover-avatar').on(hasTouch ? 'touchstart' : 'click', function (e) {
-    $(this).toggleClass('hover');
-    e.preventDefault();
-});
+    /*
+    <link rel="prefetch" href="images/empty_wide.jpg">
+    <link rel="prefetch" href="images/night_empty_wide.jpg">
+    <link rel="prefetch" href="//2.gravatar.com/avatar/5674d32fd9778602c097731984f0ec96?s=100">
+    <link rel="prefetch" href="//2.gravatar.com/avatar/5674d32fd9778602c097731984f0ec96?s=200">
+    */
 
-var nightStyle = false;
+    // when loads
 
-var updateStyle = function (night) {
-    if (night !== nightStyle) {
-        if (night === true) {
-            console.log('night');
-        } else {
-            console.log('day');
+    // $('.preload').remove();
+
+    var hasTouch = 'ontouchstart' in window;
+
+    $('.cover-avatar').on(hasTouch ? 'touchstart' : 'click', function (e) {
+        $(this).toggleClass('hover');
+        e.preventDefault();
+    });
+
+    var nightStyle = false;
+
+    var updateStyle = window._updateStyle = function (night) {
+        if (night !== nightStyle) {
+            if (night === true) {
+                $('body').removeClass('day').addClass('night');
+            } else {
+                $('body').removeClass('night').addClass('day');
+            }
+            nightStyle = night;
         }
-        nightStyle = night;
-    }
-};
+    };
 
-var checkTime = function () {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    if (hours >= 10 && hours <= 19) {
-        updateStyle(false);
-    } else {
-        updateStyle(true);
-    }
-};
+    var checkTime = function () {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        if (hours >= 10 && hours < 19) {
+            updateStyle(false);
+        } else {
+            updateStyle(true);
+        }
+    };
 
-// initial
-checkTime();
-
-// interval
-window.setInterval(function () {
-    console.log('tick');
+    // initial
     checkTime();
-}, 1 * 1000);
 
-// FIXME: end loading
+    // interval
+    window.setInterval(function () {
+        checkTime();
+    }, 1 * 1000);
+
+    // FIXME: end loading
+
+
+});
