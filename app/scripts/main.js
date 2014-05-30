a3eac25dcb669635b0bfff3e7ddede32a9fedbee
outaTiME <afalduto@gmail.com>
2014-05-30 02:57:01 -0300

    Earlier commit for new site version.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,30 +14,36 @@ $('.cover-avatar').on(hasTouch ? 'touchstart' : 'click', function (e) {
 
 var _dott;
 
-var dott = function (activate, callback) {
+var dott = function (activate) {
     if (activate !== _dott) {
         if (activate !== true) {
             // trace
             console.log('Midnight commander');
             // update image
-            $.vegas({
+            $.backstretch('images/night_empty_wide.jpg', {
+                fade: 400
+            });
+            /* $.vegas({
                 src:'images/night_empty_wide.jpg',
                 fade: 400,
                 loading: false,
                 complete: callback
-            });
+            }); */
             // attach classes
             $('body').removeClass('dott').addClass('mc');
         } else {
             // trace
             console.log('Day of the tentacle');
             // update image
-            $.vegas({
+            $.backstretch('images/empty_wide.jpg', {
+                fade: 400
+            });
+            /* $.vegas({
                 src:'images/empty_wide.jpg',
                 fade: 400,
                 loading: false,
                 complete: callback
-            });
+            }); */
             // attach classes
             $('body').removeClass('mc').addClass('dott');
         }
@@ -45,9 +51,9 @@ var dott = function (activate, callback) {
     }
 };
 
-var check = function (callback) {
+var check = function () {
     // always dott
-    dott(true, callback || $.noop);
+    dott(true);
 };
 
 var init = function () {
@@ -60,29 +66,28 @@ var init = function () {
         console.log('Dawn: %s', times.dawn);
         console.log('Dusk: %s', times.dusk);
         // override
-        check = function (callback) {
+        check = function () {
             var current = new Date().getTime();
             var dawn = times.dawn.getTime();
             var dusk = times.dusk.getTime();
-            dott(current >= dawn && current <= dusk, callback || $.noop);
+            dott(current >= dawn && current <= dusk);
         };
     } else {
         console.log('No location found');
     }
     // set initial bg
-    check(function () {
-        // end first load
-        var interval = window.setInterval(function () {
-            check();
-        }, 1 * 1000);
-        // test
-        window._mc = function (enable) {
-            window.clearInterval(interval);
-            dott(!enable);
-        };
-        // hide loader and show container
-        $('.blocking-overlay, .container').addClass('done');
-    });
+    check();
+    // end first load
+    var interval = window.setInterval(function () {
+        check();
+    }, 1 * 1000);
+    // test
+    window._mc = function (enable) {
+        window.clearInterval(interval);
+        dott(!enable);
+    };
+    // hide loader and show container
+    $('.blocking-overlay, .container').addClass('done');
 };
 
 // proload images
