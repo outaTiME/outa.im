c844e44ae868a35ab267e5b2700ed96d0043abed
outaTiME <afalduto@gmail.com>
2014-09-22 22:39:43 -0300

    Version 0.0.3 bump.

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,6 +317,12 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= config.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/fontawesome',
+          src: ['fonts/*'],
+          dest: '<%= config.dist %>'
         }]
       },
       styles: {
